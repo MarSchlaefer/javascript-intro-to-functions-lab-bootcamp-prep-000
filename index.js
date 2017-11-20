42f00d28c261bec27f8ae372ec3b52b8f47fc692
Mariel Schlaefer <mschlaefer00@gmail.com>
2017-11-20 17:11:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
  if(string === "I love you, Grandma.")
  return "I love you, too."
- if(string
+ if(string.uppercase) {
  return "YES INDEED!"
- else(string.lowercase)
+ }
+ else(string.lowercase) {
  return "I can't hear you!"
+ }
 }
