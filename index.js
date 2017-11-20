edec3a1ea10daee06c64b8d1850b736af87e3654
Mariel Schlaefer <mschlaefer00@gmail.com>
2017-11-20 17:15:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function sayHiToGrandma(string) {
  if(string === "I love you, Grandma.") {
  return "I love you, too."
  }
- if(string === string.toUpperCase) {
+ if(string === string.toUpperCase()) {
  return "YES INDEED!"
  }
- else(string === string.toLowerCase) 
+ else(string === string.toLowerCase()) 
  return "I can't hear you!"
 }
